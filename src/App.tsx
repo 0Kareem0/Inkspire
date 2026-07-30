aa6e678cee3fd3e4d5b540d9ae526280afecfce8
Elite <kimo123kimo321@gmail.com>
2026-07-30 23:42:04 +0300

    made the design ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-
+    console.log("made the design ✅")
   return (
     <>
      <p className="text-white text-8xl min-h-screen flex items-center justify-center">Inkspire incoming.....</p>
