9531ce77e91dd10aa02e0c42cc2150c61dc67d0b
Elite <kimo123kimo321@gmail.com>
2026-08-24 23:47:30 +0300

    routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import ArticleCard from "../components/ArticleCard";
 import Navbar from "../components/Navbar";
 import Categories from "../components/Categories";
 import Cards from "../components/Cards"
+import { Route, Routes } from "react-router-dom";
 
 export default function Home() {
     return(
@@ -10,6 +11,13 @@ export default function Home() {
             <ArticleCard/>
             <Categories/>
             <Cards />
+            <Routes>
+                <Route path="/articles" element={<Cards />} />
+                <Route path="/search" element={<Cards />} />
+                <Route path="/categories" element={<Cards />} />
+                <Route path="/saved" element={<Cards />} />
+
+            </Routes>
         </>
     );
 }
