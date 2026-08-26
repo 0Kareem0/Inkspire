540cfcdce34cb5301d8a03fda292ea1fefdbae2c
Elite <kimo123kimo321@gmail.com>
2026-08-26 16:11:16 +0300

    back

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log("cant focus cuz im fighting for my sanity");
+console.log("back");
 
 function App() {    
     return (
