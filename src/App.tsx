8b7af8dc6ad462415d2fe89920820ee8418d6f61
Elite <kimo123kimo321@gmail.com>
2026-08-29 23:10:58 +0300

    tired

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log("trying to get my focus back");
+console.log("im tired");
 
 function App() {    
     return (
