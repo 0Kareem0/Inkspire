51b040778474a32e1c8878df1b271b46033a4f61
Elite <kimo123kimo321@gmail.com>
2026-09-01 23:52:29 +0300

    55%

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`50%`);
+console.log(`55%`);
 
 function App() {    
     return (
