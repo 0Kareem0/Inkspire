837449c57ad9063f322dde2ffde769e824cf1192
Elite <kimo123kimo321@gmail.com>
2026-09-09 09:47:20 +0300

    Choosing whether code executes based on something being true/false

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`What does return actually do?`);
+console.log(`What is a condition?`);
 
 function App() {    
     return (
