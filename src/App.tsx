175e538f6d5bf72f12130a6937bf5e0b78faf4db
Elite <kimo123kimo321@gmail.com>
2026-09-09 09:48:32 +0300

    An ordered collection of values

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`What is an object?`);
+console.log(`What is an array?`);
 
 function App() {    
     return (
