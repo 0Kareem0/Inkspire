47798f98d42b4ace818395cfdfe02470f9dbbab3
Elite <kimo123kimo321@gmail.com>
2026-09-09 09:53:22 +0300

    Stack Frame Creation : A new stack frame is pushed onto the call stack to manage the function's execution context Data Storage: This frame stores the function's parameters, local variables, and the return address Execution: The program runs the function's instructions. If the function calls other functions, new stack frames are added on top, following the Last In, First Out (LIFO) principle Return and Cleanup : Upon completion, the function returns a value (if applicable), and its stack frame is popped from the stack. Control returns to the calling code at the stored return address, allowing the program to continue from where it left off

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`What is an error?`);
+console.log(`What does a program actually do from start → finish?`);
 
 function App() {    
     return (
