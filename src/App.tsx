b30201bb6185201c8b38901bf2a1e6a1194c823c
Elite <kimo123kimo321@gmail.com>
2026-09-09 09:47:42 +0300

    Repeating code while a condition says to continue

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`What is a condition?`);
+console.log(`What is a loop?`);
 
 function App() {    
     return (
