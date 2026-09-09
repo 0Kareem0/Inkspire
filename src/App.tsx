6fa3e53aaab5ec68d8669a34e835b3058fcae27e
Elite <kimo123kimo321@gmail.com>
2026-09-09 09:46:16 +0300

    A value you give to a function when calling it

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`What is a function?`);
+console.log(`What is an argument?`);
 
 function App() {    
     return (
