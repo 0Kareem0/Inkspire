51b64567ac608fb55b49f79aa4449d8432d9590b
Elite <kimo123kimo321@gmail.com>
2026-09-19 18:09:16 +0300

    CPU

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`ground up`);
+console.log(`CPU`);
 
 function App() {    
     return (
