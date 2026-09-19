c0e4a457efd8fcde692965c42f333d8403a75ff6
Elite <kimo123kimo321@gmail.com>
2026-09-19 18:09:30 +0300

    OS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Write from "./pages/Write";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import { Route, Routes } from "react-router-dom";
-console.log(`CPU`);
+console.log(`OS`);
 
 function App() {    
     return (
